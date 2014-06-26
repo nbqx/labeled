f8dca148df3b57fdc61aef7795733106c6ffc099
n.b.q.x <nbqxnbq@gmail.com>
2014-06-26 14:00:45 +0900

    add no args action

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 var labeled = require(__dirname+"/../");
 
-var color = process.argv[2] || "red";
-labeled(color).on('data',function(data){
-  if(data!==''){
-    console.log(data);
-  }
-});
+var color = process.argv[2];
+if(color!==undefined){
+  labeled(color).on('data',function(data){
+    if(data!==''){
+      console.log(data);
+    }
+  });
+}else{
+  require('fs').createReadStream(__dirname+"/../h.txt").pipe(process.stdout);
+}
